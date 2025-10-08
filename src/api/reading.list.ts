fe8f78d0e70cb19cdbffe6667c8329eb864e3dde
Fambegbe Olamileke <olamileke.f@gmail.com>
2025-10-08 19:58:52 +0100

    fix: minor fixes

--- src/api/reading.list.ts
+++ src/api/reading.list.ts
@@ -2,7 +2,10 @@ import { store } from '@/store'
 import type { TReadingListItem } from '@/types/reading.list'
 
 export const useGetReadingList = (func: (items: TReadingListItem[]) => void) => {
-  if (store['reading-list']) return store['reading-list']
+  if (store['reading-list']) {
+    func(store['reading-list'])
+    return
+  }
 
   fetch(`${import.meta.env?.VITE_API_URL}/reading-list`)
     .then(async (response) => {
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Categories from '@/views/Categories.vue'
 import Series from '@/views/Series.vue'
 import SeriesReviews from '@/views/SeriesReviews.vue'
 import About from '@/views/About.vue'
+import Review from '@/views/Review.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,12 +14,17 @@ const router = createRouter({
       name: 'Home',
       component: Home,
       children: [
-        { path: '', component: Categories },
-        { path: '/category/:slug', component: Categories },
-        { path: '/series/:slug', component: SeriesReviews },
-        { path: '/series', component: Series },
-        { path: '/about', component: About },
+        { path: '/', name: '', component: Categories },
+        { path: '/category/:slug', name: 'Categories', component: Categories },
+        { path: '/series/:slug', name: 'Series Reviews', component: SeriesReviews },
+        { path: '/series', name: 'Series', component: Series },
+        { path: '/about', name: 'About', component: About },
       ],
+    },
+    {
+      path: '/:slug',
+      name: 'Review',
+      component: Review,
     },
   ],
 })
