234fbefa72fc78fe8bc05c85f7065dc86232ec2b
Fambegbe Olamileke <olamileke.f@gmail.com>
2025-10-13 18:31:50 +0100

    feat: working on review page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { RouterLink } from 'vue-router'
+import { RouterLink, useRoute } from 'vue-router'
 import Button from '@/components/Button.vue'
 import SubscribeDialog from '@/components/SubscribeDialog.vue'
+
+const route = useRoute()
+const isReviewPage = route.name.toLowerCase() === "review" ? true : false; 
+console.log(isReviewPage)
 
 const isDialogOpen = ref(false);
 </script>
 
 <template>
-  <nav class="w-screen border-b-[1px] border-b-[rgba(0,0,0,0.05)]">
+  <nav class="border-b-[1px] border-b-[rgba(0,0,0,0.05)]">
     <div class="max-w-[1200px] mx-auto py-4 flex justify-between items-center">
-      <RouterLink to="/" class="relative right-[5px]">
-        <img src="https://res.cloudinary.com/olamileke/image/upload/c_thumb,w_200,g_face/v1760027186/chequer_dev/ol-high-resolution-logo_fmyams.png" class="size-9 rounded-sm" />
+      <RouterLink to="/" class="relative" :class="isReviewPage ? '' : ''">
+        <img src="https://res.cloudinary.com/olamileke/image/upload/c_thumb,w_200,g_face/v1760027186/chequer_dev/ol-high-resolution-logo_fmyams.png" class="size-9 object-cover" />
       </RouterLink>
 
       <Button @click="isDialogOpen = true">Subscribe</Button>
--- src/components/RelatedSeriesReviews.vue
+++ src/components/RelatedSeriesReviews.vue
@@ -1,11 +1,14 @@
 <script lang="ts" setup>
 import { ref } from "vue";
+import { RouterLink } from "vue-router"
 import { useGetRelatedSeriesReviews } from "@/api/review"
 import { TReview } from "@/types/review"
 import { TSeries } from "@/types/series"
+import Review from "@/components/Review.vue"
+import { parseDate } from '@/utilities/date'
 
-const props = defineProps<{ review_uuid: string, series: Pick<TSeries, "uuid" | "name"> }>();
-const reviews = ref<Pick<TReview, "title" | "image" | "author" | "created_at">>();
+const { review_uuid, series } = defineProps<{ review_uuid: string, series: Pick<TSeries, "uuid" | "name"> }>();
+const reviews = ref<TReview | undefined>();
 
 const updateReviews = (fetchedReviews: Pick<TReview, "title" | "image" | "author" | "created_at">) => {
     reviews.value = fetchedReviews;
@@ -15,10 +18,16 @@ useGetRelatedSeriesReviews(review_uuid, series.uuid, updateReviews)
 </script>
 
 <template>
-    <template v-if="reviews">
-
-    </template>
-    <template v-else>
-        There are no other reviews
-    </template>
+    <section class="bg-[rgba(0,0,0,0.005)] text-[rgba(0,0,0,0.75)] p-18">
+        <div class="max-w-[750px] mx-auto">
+            <template v-if="reviews">
+                <p class="text-lg text-center font-semibold mb-16">More from <span class="capitalize">{{ series.name }}</span>
+                </p>
+                <Review v-for="(review, index) in reviews" :key="index" v-bind="review" class="mb-8 ml-[25px] last-of-type:mb-0" />
+            </template>
+            <template v-else>
+                There are no other reviews
+            </template>
+        </div>  
+    </section>
 </template>
